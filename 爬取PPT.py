f21d9261fb281e940eb116073b1f26c703692fa9
贾云嵩 <51917607+JiaYunSong@users.noreply.github.com>
2020-03-26 10:29:18 +0800

    Add files via upload

---
+++ 爬取PPT.py
@@ -0,0 +1,33 @@
+import os
+import requests
+
+
+def getImage(url):
+    try:
+        kv = {'user-agent': 'Mozilla/5.0'}
+        r = requests.get(url, headers=kv, timeout=30)
+        if r.status_code != 200:
+            raise Exception(f"code={r.status_code}")
+        return r.content
+    except Exception as e:
+        print(f"Request ERROR: {e} // URL: {url}")
+        exit(0)
+
+
+f = open(r"source.txt", 'r', encoding='utf-8')
+source = f.read()
+source = source[source.find('= \"'):source.find('\";')][3:]
+img_path = source.replace('\n', '').split(',')
+
+if not os.path.exists('img'):
+    os.mkdir('img')
+
+for i in range(len(img_path)):
+    r = requests.get(img_path[i])
+    img = r.content
+    with open(f'img/{i}.png', 'wb') as f:
+        f.write(img)
+    print(f"第{i}页完成获取")
+
+# 不用尝试多线程，速度一样烂
+# Copylefts.RSMX.CIEE-CAU.2020-03-25
